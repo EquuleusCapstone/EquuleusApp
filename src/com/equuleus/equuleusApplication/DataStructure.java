0631609b87a19f6d1eb62203b1d5c581eb0a710b
jtadamec <jamestadamec@gmail.com>
2014-04-15 13:01:50 -0700

    Revert "Testing commit. Added comment about error in DataStructure.java"
    This reverts commit 5e5e577c114d5ee2094cd1957f22a7b9d981457c.

--- src/com/equuleus/equuleusApplication/DataStructure.java
+++ src/com/equuleus/equuleusApplication/DataStructure.java
@@ -25,7 +25,7 @@ public class DataStructure {
 	
 	public void delete(String nameIn)
 	{
-		int delIndex = search(nameIn); //Has been throwing an IndexOutOfBoundsException when called from the meeting screen
+		int delIndex = search(nameIn);
 		swap(delIndex, tailIndex);
 		array.remove(tailIndex);
 		tailIndex--;
